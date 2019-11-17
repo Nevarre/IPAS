fac7d883ddab09d5264d5c8c5030e9d449418abb
Jiani Chen <jchen@blpc0.bl.pvt>
2019-11-16 18:47:18 -0800

    Fix bug in interactive flagging
    Numpy arrays are pointer objects...

--- interactive_plotting/interactive_plot.py
+++ interactive_plotting/interactive_plot.py
@@ -31,16 +31,16 @@ cmap = "hot"
 rawdatafile = filterbank.FilterbankFile("/mnt_blpd9/datax/incoming/spliced_guppi_57991_49905_DIAG_FRB121102_0011.gpuspec.0001.8.4chan.fil")
 data, bins, nbins, start_time = wt.waterfall(rawdatafile, start, duration, dm=dm, nsub=nsub,  width_bins=width)
 
-
 # data points for plotting
 values = data.data
-current_values = values
+current_values = np.copy(values)
 
 time_signal = np.sum(values, axis=0).flatten()
 current_time_signal = time_signal
 
 vmin = np.mean(values)
 inc = np.std(values)/10
+
 
 def average(x, y):
     return (x+y)/2
@@ -88,8 +88,12 @@ def replot():
     """Re-plots and updates the current canvas with the new values."""
     global fig, ax, current_values, current_time_signal
 
-    ax[0].plot(current_time_signal, color='k')
+    ax[0].cla()
+    ax[0].plot(current_time_signal, color='k', scalex=True)
+    ax[0].set_xlim(0, len(current_time_signal))
+
     ax[1].imshow(current_values, vmin=vmin, origin='lower', aspect='auto', cmap=cmap)
+    ax[1].set(ylabel = 'Frequency', xlabel='Time')
     fig.canvas.draw()
 
 def on_key(event):
@@ -134,7 +138,7 @@ def check_state(state):
 
     if state == 0:
         print('Returning to original plot')
-        current_values = values
+        current_values = np.copy(values)
         current_time_signal = time_signal
         replot()
     elif state == 1:
@@ -180,13 +184,14 @@ def main():
 
     wt.plot_waterfall(data, start, duration, dm, "unknown_cand", cmap_str=cmap, sweep_posns=sweep_posn)
 
-    fig, ax  = plt.subplots(2, sharex=True, gridspec_kw={'height_ratios':[1,3]})
+    fig, ax  = plt.subplots(2, gridspec_kw={'height_ratios':[1,3]})
 
-    ax[0].plot(time_signal, color='k')
+    ax[0].plot(time_signal, color='k', scalex=True)
+    ax[0].set_xlim(0, len(current_time_signal))
     #ax[0].axes.get_yaxis().set_ticks([])
     #ax[0].axes.get_xaxis().set_ticks([])
 
-    ax[1].imshow(values, vmin=vmin, origin='lower', aspect='auto', cmap=cmap)
+    ax[1].imshow(current_values, vmin=vmin, origin='lower', aspect='auto', cmap=cmap)
     ax[1].set(ylabel = 'Frequency', xlabel='Time')
 
     fig.canvas.mpl_connect('key_press_event', on_key)
